f2d65a34b49c79fc05ffb5c7efd001f357d9fc49
Pedro Janneo <pdro.henriquejanneo@gmail.com>
2025-08-29 10:32:37 -0300

    Initial commit

---
+++ src/main/java/com/bookStoreJava/BookStoreJava/BookStoreJavaApplication.java
@@ -0,0 +1,13 @@
+package com.bookStoreJava.BookStoreJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreJavaApplication.class, args);
+	}
+
+}
