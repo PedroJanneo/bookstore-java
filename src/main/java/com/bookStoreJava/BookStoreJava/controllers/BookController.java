0965b8efe0eeb39e43e8aae7f4c87ad32fe4d176
Pedro Janneo <pdro.henriquejanneo@gmail.com>
2025-09-05 09:35:02 -0300

    feat: POST livros

--- src/main/java/com/bookStoreJava/BookStoreJava/controllers/BookController.java
+++ src/main/java/com/bookStoreJava/BookStoreJava/controllers/BookController.java
@@ -2,10 +2,14 @@ package com.bookStoreJava.BookStoreJava.controllers;
 
 
 import com.bookStoreJava.BookStoreJava.entities.Book;
+import com.bookStoreJava.BookStoreJava.entities.User;
 import com.bookStoreJava.BookStoreJava.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,6 +33,12 @@ public class BookController {
     public Book findById(@PathVariable Long id){
         Book result = bookService.findByID(id);
         return result;
+    }
+
+    @PostMapping
+    public ResponseEntity<Book> createUser(@RequestBody Book book) {
+        Book savedBook = bookService.save(book);
+        return ResponseEntity.ok(savedBook);
     }
 
 }
